9d3609ec6a5595a469fd39b2af166f3335c61e05
github username <github email address>
2021-09-27 12:23:21 -0400

    Done

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function findMinAndRemove(array){
+    let min = array[0]
+    let minDex = 0
 
+    for(let i = 1; i < array.length; i ++) {
+        if(array[i] < min) {
+            minDex = i
+            min = array[i]
+        }
+    }
+
+    array.splice(minDex, 1)
+    return min
 }
 
 function selectionSort(array){
+    let sortedArr = []
 
+    while(array.length) {
+        sortedArr.push(findMinAndRemove(array))
+    }
+
+    return sortedArr
 }
+
+console.log(selectionSort([3, 2, 6, 1]))
